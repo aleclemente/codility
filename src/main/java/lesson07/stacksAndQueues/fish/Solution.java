729f1c8531c080375f61dde84844a7c0d1fd0246
aecm <alexandreclemente@ext.cvccorp.com.br>
2023-01-20 11:12:13 -0300

    Fish solution and test solvec 100%

--- src/main/java/lesson07/stacksAndQueues/fish/Solution.java
+++ src/main/java/lesson07/stacksAndQueues/fish/Solution.java
@@ -3,31 +3,44 @@ package lesson07.stacksAndQueues.fish;
 import java.util.Stack;
 
 class Solution {
-    public int solution(int[] A, int[] B){
+    public int solution(int[] fishSize, int[] fishDirection) {
 
-        if(A.length == 1) {
+        if (fishSize.length == 1 || fishDirection.length == 1) {
             return 1;
         }
 
-        Stack<Integer> fishsStack = new Stack<>();
+        Stack<Integer> survivalsDownstreamIndex = new Stack<>();
 
-        fishsStack.push(0);
+        int survivalsUpstream = 0;
 
-        for(int i=1; i < A.length; i++) {
+        for (int i = 0; i < fishSize.length; i++) {
 
-            //System.out.println( "\nPeixe " + fishsStack.peek() + ": " + A[fishsStack.peek()] + " " + ( B[fishsStack.peek()] == 0 ? "A" : "v" ) + " / Peixe " + i + ": " + A[i] + " " + ( B[i] == 0 ? "A" : "v" ) );
+            int size = fishSize[i];
+            int direction = fishDirection[i];
+            boolean survives = true;
 
-            if (B[fishsStack.peek()] == B[i] || (B[fishsStack.peek()] == 0 && B[i]==1) ) {
-                fishsStack.push(i);
-            } else if (A[fishsStack.peek()] < A[i] ){
-                fishsStack.pop();
-                fishsStack.push(i);
+            if (direction == 0) {
+
+                while (!survivalsDownstreamIndex.empty()) {
+
+                    if (size > fishSize[survivalsDownstreamIndex.peek()]) {
+                        survivalsDownstreamIndex.pop();
+                    } else {
+                        survives = false;
+                        break;
+                    }
+
+                }
+
+                if(survives){
+                    survivalsUpstream++;
+                }
+            } else {
+                survivalsDownstreamIndex.push(i);
             }
-
-            //System.out.println( "\nPeixe " + fishsStack.peek() + ": " + A[fishsStack.peek()] + " " + ( B[fishsStack.peek()] == 0 ? "A" : "v" ) + " / Peixe " + i + ": " + A[i] + " " + ( B[i] == 0 ? "A" : "v" ) );
 
         }
 
-       return fishsStack.size();
+        return survivalsDownstreamIndex.size() + survivalsUpstream;
     }
 }
