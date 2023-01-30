4d40fc00c53eae230572e327efdc870a8cd94236
aecm <alexandreclemente@ext.cvccorp.com.br>
2023-01-30 12:14:01 -0300

    Refactored with description until lesson 05

--- src/main/java/exam01/Solution.java
+++ src/main/java/exam01/Solution.java
@@ -20,3 +20,19 @@ class Solution {
         return steps;
     }
 }
+/*
+Recebe um número binário transforma em decimal e verifica se esse é par, caso positivo divida por dois, caso seja impar subtrai por um.
+Conte quando passos foram necessários
+
+Por exemplo:
+011100 = 28
+Número par, divide por 2 = 14
+Número par, divide por 2 = 7
+Número impar, subtrai 1 = 6
+Número par, divide por 2 = 3
+Número impar, subtrai 1 = 2
+Número par, divide por 2 = 1
+Número impar, subtrai 1 = 0
+
+Resposta: 7 Passos
+ */
